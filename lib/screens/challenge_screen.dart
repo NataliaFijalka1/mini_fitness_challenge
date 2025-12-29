167f854286865c84bca2021709575f39fe5bd51c
NataliaFijalka1 <fhb232581@fh-vie.ac.at>
2025-12-29 16:54:55 +0100

    INteraktiven Herasforderungsablauf + Timer/ Zähler + Fortschrittsanzeige hinzugefügt (getestet) und Filenamen ausgebessert

--- lib/screens/challenge_screen.dart
+++ lib/screens/challenge_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../models/challenge.dart';
-import '../sevices/challange_generator.dart';
+import '../sevices/challenge_generator.dart';
 import 'result_screen.dart';
+import 'dart:async';
 
 class ChallengeScreen extends StatefulWidget {
   static const routeName = '/challenge';
@@ -11,11 +12,22 @@ class ChallengeScreen extends StatefulWidget {
 @override
   State<ChallengeScreen> createState() => _ChallengeScreenState();
 }
+
+
+
+
 class _ChallengeScreenState extends State<ChallengeScreen> {
   final _generator = ChallengeGenerator();
+  Timer? _timer;
 
   late Difficulty _difficulty;
   late Challenge _challenge;
+
+
+  bool _running = false;
+  int _repsDone = 0;
+  int _secondsPassed = 0;
+  int _pauseCount = 0;
 
   @override
 void didChangeDependencies() {
@@ -27,37 +39,121 @@ void didChangeDependencies() {
     _challenge = _generator.generate(_difficulty); //einmalig
   }
 
-
-
-int _calculateScore(Challenge c) {
-    final base = c.kind == ChallengeKind.reps ? c.target : (c.target / 2).round();
-    return base * c.difficulty.multiplier;
-    // daweil:
-    // reps: target * multiplier
-    // timer: (target/2) * multiplier (weil Sekunden tendenziell größer sind)
+@override
+void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
-  String _goalText(Challenge c) {
-    return c.kind == ChallengeKind.reps
-        ? '${c.target} Wiederholungen'
-        : '${c.target} Sekunden';
-  } 
 
 
+
+  int get _currentValue => _challenge.kind == ChallengeKind.reps ? _repsDone : _secondsPassed;
+
+  double get _progress => (_currentValue / _challenge.target).clamp(0,1);
+
+  String get _goalText => _challenge.kind == ChallengeKind.reps
+      ? '${_challenge.target} Wiederholungen'
+      : '${_challenge.target} Sekunden';
+
+int _calculateScore() {
+    final base = _challenge.kind == ChallengeKind.reps
+        ? _challenge.target 
+        : (_challenge.target / 2).round();
+   
+final penalty = _pauseCount * 3;
+
+final rawScore = base * _challenge.difficulty.multiplier - penalty;
+return rawScore < 0 ? 0 : rawScore;
+
+  }
+
+
+
+void _start() {
+  setState(() => _running = true);
+
+
+  if (_challenge.kind == ChallengeKind.timer) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+     if (!_running) return;
+
+      setState(() =>
+        _secondsPassed++);
+        if (_secondsPassed >= _challenge.target) {
+          _finish();
+        }
+    });
+  }
+}
+
+void _pause() {
+  if (!_running) return;
+    setState(() {
+      _running = false;
+      _pauseCount++;
+    });
+  }
+
+void _abbort() {
+    _timer?.cancel();
+    _timer = null;
+    Navigator.pop(context);
+  }
+
+void _addRep() {
+  if (!_running) return;
+  if (_challenge.kind != ChallengeKind.reps) return;
+
+    setState(() => _repsDone++);
+      if (_repsDone >= _challenge.target) {
+        _finish();
+      }
+    }
+
+    void _finish() {
+    _timer?.cancel();
+    _timer = null;
+    setState(() =>
+      _running = false);
+
+      Navigator.pushReplacementNamed(
+        context, 
+        ResultScreen.routeName,
+        arguments: ResultArgs(
+          challenge: _challenge,
+          score: _calculateScore(),
+        ),
+      );
+    }
+
+    void _newChallenge() {
+      _timer?.cancel();
+      _timer = null;
+
+      setState(() {
+      _challenge = _generator.generate(_difficulty);
+      _repsDone = 0;
+      _secondsPassed = 0;
+      _pauseCount = 0;
+      _running = false;
+    });
+    }
 
 @override
   Widget build(BuildContext context) {
-    final score = _calculateScore(_challenge);
+      final current = _currentValue;
 
-    return Scaffold (appBar: AppBar(title: const Text ('Challange'),
-    ),
+    return Scaffold (appBar: AppBar(title: const Text ('Challange')),
     body: Padding(
       padding: const EdgeInsets.all(16),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [ Text (
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, 
+        children: [ Text (
         'Schwierigkeitsgrad: ${_difficulty.label}',
         style: Theme.of(context).textTheme.titleMedium,
         ),
-        const SizedBox(height: 16),
+        const SizedBox(height: 12),
 
 
         Card(
@@ -66,49 +162,81 @@ int _calculateScore(Challenge c) {
           children: [
             Text(
               _challenge.title,
+              style: Theme.of(context).textTheme.headlineSmall),
+            
+            const SizedBox(height: 8),
+            Text('Ziel: $_goalText'),
+            const SizedBox(height: 12),
+            LinearProgressIndicator(
+              value: _progress),
+            const SizedBox(height: 12),
+            Center (
+              child: Text('$current / ${_challenge.target}',
               style: Theme.of(context).textTheme.headlineSmall,
+              ),
             ),
             const SizedBox(height: 8),
-            Text('Type: ${_challenge.kind.label}'),
-            const SizedBox(height: 4),
-            Text('Ziel: ${_goalText(_challenge)}'),
-            const SizedBox(height: 12),
-            Text('Punkte: $score'),
-            
-          ],),
+            Text('Pausen: $_pauseCount'),
+          ],
+          ),
         ),
+        ),
+
+        const SizedBox(height: 12),
+
+        if(_challenge.kind == ChallengeKind.reps)
+        FilledButton.icon(
+          onPressed: _running ? _addRep : null,
+          icon: const Icon(Icons.add),
+          label: const Text('Rep +1'),
         ),
 
         const Spacer(),
 
-        OutlinedButton.icon(
-          onPressed: () {
-            setState(() {
-              _challenge = _generator.generate(_difficulty);
-            });
-          },
-          icon: const Icon(Icons.shuffle),
-          label: const Text('Neue Challenge generieren'),
-        ),
-        const SizedBox(height: 8),
-
-
-
-        FilledButton.icon(onPressed: () {
-          Navigator.pushNamed(
-            context,
-            ResultScreen.routeName,
-            arguments: ResultArgs(
-              challenge: _challenge,
-              score: score,
+        Row(
+          children: [
+            Expanded(child:
+            FilledButton(
+              onPressed: _running? null : _start,
+              child: const Text('Start'),
             ),
-            );
-          
-        },
-        icon: const Icon(Icons.check),
-        label: const Text('Challenge abgeschlossen'),
+            ),
+            const SizedBox(width: 8),
+            Expanded(child:
+            OutlinedButton(
+          onPressed: _running ? _pause : null,
+          child: const Text('Pause'),
+         
         ),
-      ],
+            ),
+          ],
+        ),
+        Row(
+     children: [
+        Expanded(
+          child: OutlinedButton.icon(
+            onPressed: _newChallenge,
+          icon: const Icon(Icons.shuffle),
+            label: const Text('Neue Challenge generieren'),
+          ),
+        ),
+
+        const SizedBox(height: 8),
+        Expanded(
+          child: TextButton(
+            onPressed: _abbort,
+            child: const Text('Abbrechen'),
+          ),
+        ),
+     ],
+    ),
+
+        
+
+
+
+
+        ],
       ),
     ),
     );
